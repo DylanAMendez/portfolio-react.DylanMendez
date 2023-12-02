e6dfa2a4806a752c0464c89f1ed0f370e3fbf4cb
DylanAMendez <dylanmendez07@hotmail.com>
2023-12-02 15:54:46 -0300

    Arreglando Nav  y HeroContainer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import logoDM from '../assets/images/DMLogo.png'
 export const Header = () => {
   return (
     <header className="flex place-content-between items-center mb-8 bg-[#000000b7] "  id='header-id'>
-        <img src={logoDM} alt="Logo" />
+        <img src={logoDM} alt="Logo" className='max-h-16 lg:max-h-32' />
         
         <NavBar />
         
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,23 +18,31 @@ export const NavBar = () => {
   return (
     <>
     <ul className={`${menuClicked ? 'hidden' : ''} absolute text-[#23714D] top-0 right-0 w-[256px] p-[24px] h-full text-[18px] sm:flex sm:items-center sm:w-[438px] sm:place-content-around sm:p-0 sm:h-auto sm:relative sm:text-[16px] mr-1 bg-[#000000f5] lg:bg-[#00000000] lg:mx-7 z-10 font-fontPoppins  `}>
+
         <li className={`${menuClicked ? 'hidden' : ''} cursor-pointer sm:hidden  flex place-content-end`}>
           <img className='w-8 h-8 mb-[87px]' src={closeBtn} onClick={handleClick} alt="" />
         </li>
+
         <li className='mb-8 sm:mb-0'>
             <Link to="/">Home</Link>
         </li>
+
         <li className='mb-8 sm:mb-0'>
         <Link to="/projects">Projects</Link>
         </li>
+
         <li className='mb-8 sm:mb-0'>
         <Link to="/services">Services</Link>
         </li>
+
         <li className='mb-8 sm:mb-0'>
-        <Link to="/contact">Contacto</Link>
+        <a href="#contact">Contacto</a>
         </li>
+
       </ul>
+
       <img className={`${menuClicked ? '' : 'hidden'} w-10 h-5 mr-9 cursor-pointer sm:hidden`} src={burgerMenu} onClick={handleClick} alt="" />
+      
     </>
   )
 }
