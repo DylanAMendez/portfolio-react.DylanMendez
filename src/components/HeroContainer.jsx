bdfa1d8498457137af33d37ef287abe80b5b9cf0
DylanAMendez <dylanmendez07@hotmail.com>
2023-12-05 20:13:11 -0300

    Actualizando CV

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -27,7 +27,7 @@ export const HeroContainer = () => {
             <strong className="hidden lg:flex lg:ml-1 text-[#e6ffe6] "> LinkedIn </strong>
            </a>
 
-            <a href="https://docs.google.com/file/d/1QrCNez7OQtIkW3oCxwLraedqkt7mgzPoscx_r2zFkkg/view" rel="noreferrer" target="_blank" className=" flex  transition-all duration-100 ease-in-out hover:border-b-2 hover:border-[#e6ffe6] hover:scale-110" >
+            <a href="https://drive.google.com/file/d/1E_4IUoKXZm2aB7Bxc8BE08s9yDq6I-Im/view?usp=sharing" rel="noreferrer" target="_blank" className=" flex  transition-all duration-100 ease-in-out hover:border-b-2 hover:border-[#e6ffe6] hover:scale-110" >
             <svg xmlns="http://www.w3.org/2000/svg" height="27" width="27" viewBox="0 0 384 512"><path fill="#4fc990" d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
             <strong className="hidden lg:flex lg:ml-1 text-[#e6ffe6] "> Resume </strong>
               </a>
