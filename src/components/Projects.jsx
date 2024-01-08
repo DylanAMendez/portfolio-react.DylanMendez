233f9f8b17c4edc303f5e380ec790a46f9855eab
DylanAMendez <dylanmendez07@hotmail.com>
2024-01-08 20:17:33 -0300

    update componente projects - Binary App

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,8 @@ import MuroDeFirmas from "../datos/MuroDeFirmas_ProyectoPersonal.js";
 import MuroDeFirmasImage from '../assets/images/MuroDeFirmas-viewMobile.png'
 
 // project moreno aberturas
-const projectComponents = lastProjects.map((project) => {
+const projectComponents = lastProjects.map((project) =>
+{
   const { name, imageUno, imageDos, imageTres, description, link } = project;
 
   return {
@@ -38,7 +39,7 @@ const proyectosPersonalesComponents = proyectosPersonales.map((personal) =>
 {
 
   const { name, image, description, descriptionUno, descriptionDos, link } = personal;
-  
+
   return { name, image, description, descriptionUno, descriptionDos, link, };
 
 });
@@ -51,17 +52,18 @@ const todoListComponents = todoList_ProyectoPersonal.map((personal) =>
 
   return { name, image, description, descriptionUno, descriptionDos, link };
 
-} )
+})
 
 // project personal Muro de Firmas
-const muroDeFirmasComponent = MuroDeFirmas.map ((personal) =>
+const muroDeFirmasComponent = MuroDeFirmas.map((personal) =>
 {
   const { name, image, description, descriptionUno, descriptionDos, descriptionTres, descriptionCuatro, link } = personal;
 
   return { name, image, description, descriptionUno, descriptionDos, descriptionTres, descriptionCuatro, link };
 })
 
-export const Projects = () => {
+export const Projects = () =>
+{
   return (
     <div>
       <VideoBack />
@@ -74,7 +76,7 @@ export const Projects = () => {
           </h2>
 
           <p className="mt-10 pb-5 text-[#FFFFFF] text-center m-9 font-bold lg:text-lg font-fontRoboto" id="aboutMe-p">
-          Este proyecto fue realizado en colaboración con la empresa <br /> Moreno Aberturas
+            Este proyecto fue realizado en colaboración con la empresa <br /> Moreno Aberturas
           </p>
 
           <div className="sm:grid  lg:grid lg:grid-cols-3  gap-4 items-center mb-6 pb-1">
@@ -83,206 +85,206 @@ export const Projects = () => {
 
                 <h3 className="text-[#F1CCBA] font-medium font-fontRoboto pb-5 lg:pb-7 lg:mt-3 sm:my-7">Pagina Inicio</h3>
                 <img src={morenoAberturasImagenUno} className="inline max-h-60 md:max-h-80 lg:max-h-96" alt={project.name} />
-            
+
               </div>
 
 
-        
+
             ))}
 
-                {projectComponents.map((project, index) => (
+            {projectComponents.map((project, index) => (
               <div key={index} className="mx-8 my-7 text-center content-center justify-center" id="projects-div">
 
                 <h3 className="text-[#F1CCBA] font-medium font-fontRoboto pb-5 lg:pb-7 lg:mt-3">Seccion Articulos</h3>
                 <img src={morenoAberturasImagenDos} className="inline max-h-44 md:max-h-80 lg:max-h-96" alt={project.name} />
-            
+
               </div>
 
 
-        
+
             ))}
 
-                
-                    {/* IMAGEN PROJECT 3 */}
+
+            {/* IMAGEN PROJECT 3 */}
             {projectComponents.map((project, index) => (
               <div key={index} className="mx-8 my-7 text-center content-center justify-center" id="projects-div">
 
                 <h3 className="text-[#F1CCBA] font-medium font-fontRoboto pb-5 lg:pb-7 lg:mt-3">Seccion Detalles</h3>
                 <img src={morenoAberturasImagenTres} className=" inline max-h-44 md:max-h-80 lg:max-h-96" alt={project.name} />
-            
+
               </div>
 
 
-        
+
             ))}
 
-   
+
 
           </div>
 
-            
+
           <div className=" items-center mb-6 pb-1">
-  
-         <div className=" mx-8 text-center content-center justify-center">
+
+            <div className=" mx-8 text-center content-center justify-center">
 
               <BtnInformation />
 
-          </div>
+            </div>
 
           </div>
 
 
           <div className=" items-center mb-6 pb-1">
-  
-          <div className=" mx-8 text-center content-center justify-center">
 
-          <button className="content-center sm:my-7">
-            <a href={projectComponents[0].link} target="_blank" rel="noopener noreferrer" className="text-center font-fontRoboto " id="hero-button">
-             Pagina del Proyecto 
-            </a>
-            </button>
+            <div className=" mx-8 text-center content-center justify-center">
+
+              <button className="content-center sm:my-7">
+                <a href={projectComponents[0].link} target="_blank" rel="noopener noreferrer" className="text-center font-fontRoboto " id="hero-button">
+                  Pagina del Proyecto
+                </a>
+              </button>
+
+            </div>
 
           </div>
 
-          </div>
 
-
-              {/* ---- */}
+          {/* ---- */}
 
 
           <h2 className="font-normal font-fontPoppins text-center sm:text-xl mt-10" id="aboutMe-h2">
             Proyectos Personales
           </h2>
 
-             <div className=" lg:grid lg:grid-cols-2 items-start">
+          <div className=" lg:grid lg:grid-cols-2 items-start">
 
 
-              {/* Muro de Firmas */}
+            {/* Muro de Firmas */}
 
-              {muroDeFirmasComponent.map((personal, index) => (
-            
-            <div key={index} className=" mb-5 mx-5 flex justify-center " >
+            {muroDeFirmasComponent.map((personal, index) => (
 
-      <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
-      <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={MuroDeFirmasImage}  alt="" />
+              <div key={index} className=" mb-5 mx-5 flex justify-center " >
 
-   <div className="p-5 lg:m-5 text-left ">
+                <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
+                  <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={personal.image} alt="" />
 
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> { personal.name } </h5>
+                  <div className="p-5 lg:m-5 text-left ">
 
-      <p className="mb-3 font-normal max-w-sm text-gray-700 dark:text-gray-400 lg:py-5">
-           {personal.description} <br /> <br />
-           {personal.descriptionUno} <br /> <br />
-           {personal.descriptionDos} <br />
-           {personal.descriptionTres} <br /> <br />
-           {personal.descriptionCuatro}
-      </p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {personal.name} </h5>
 
-      <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer"  target="_blank" >
-          ver pagina
-         
-          <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <p className="mb-3 font-normal max-w-sm text-gray-700 dark:text-gray-400 lg:py-5">
+                      {personal.description} <br /> <br />
+                      {personal.descriptionUno} <br /> <br />
+                      {personal.descriptionDos} <br />
+                      {personal.descriptionTres} <br /> <br />
+                      {personal.descriptionCuatro}
+                    </p>
 
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer" target="_blank" >
+                      ver pagina
 
-        </svg>
+                      <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
 
-      </a>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+
+                      </svg>
+
+                    </a>
 
 
-  </div>
-</div>
+                  </div>
+                </div>
 
-            </div>
-
-            
-      
-          ))}
+              </div>
 
 
 
+            ))}
 
 
-              {/* To do List */}
-    
-             {todoListComponents.map((personal, index) => (
-            
-            <div key={index} className=" mb-5 mx-5 flex justify-center " >
-
-      <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
-      <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={proyectoPersonalTodoList}  alt="" />
-
-   <div className="p-5 lg:m-5 text-left ">
-
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> { personal.name } </h5>
-
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 lg:py-5">
-           {personal.description} <br />
-           {personal.descriptionUno} <br />
-           {personal.descriptionDos} <br />
-      </p>
-
-      <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer"  target="_blank" >
-          ver pagina
-         
-          <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-
-        </svg>
-
-      </a>
 
 
-  </div>
-</div>
 
-            </div>
+            {/* To do List */}
 
-            
-      
-          ))}
-            
-          {proyectosPersonalesComponents.map((personal, index) => (
-            
-            <div key={index} className=" mb-5 mx-5 flex justify-center " >
+            {todoListComponents.map((personal, index) => (
 
-      <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
-      <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={proyectoPersonalCalculadora}  alt="" />
+              <div key={index} className=" mb-5 mx-5 flex justify-center " >
 
-   <div className="p-5 lg:m-5 text-left ">
+                <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
+                  <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={proyectoPersonalTodoList} alt="" />
 
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> { personal.name } </h5>
+                  <div className="p-5 lg:m-5 text-left ">
 
-      <p className="mb-3 font-normal max-w-sm text-gray-700 dark:text-gray-400 lg:py-5">
-           {personal.description} <br />
-           {personal.descriptionUno} <br />
-           {personal.descriptionDos} <br />
-      </p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {personal.name} </h5>
 
-      <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer"  target="_blank" >
-          ver pagina
-         
-          <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 lg:py-5">
+                      {personal.description} <br />
+                      {personal.descriptionUno} <br />
+                      {personal.descriptionDos} <br />
+                    </p>
 
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                    <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer" target="_blank" >
+                      ver pagina
 
-        </svg>
+                      <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
 
-      </a>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+
+                      </svg>
+
+                    </a>
 
 
-  </div>
-</div>
+                  </div>
+                </div>
 
-            </div>
-
-            
-      
-          ))}
+              </div>
 
 
-          {/* {proyectosPersonalesComponents.map((personal, index) => (
+
+            ))}
+
+            {proyectosPersonalesComponents.map((personal, index) => (
+
+              <div key={index} className=" mb-5 mx-5 flex justify-center " >
+
+                <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  flex flex-col items-center ">
+                  <img className="  max-w-xs max-h-80  lg:h-[400px] lg:max-w-lg 2xl:h-[400px] " src={proyectoPersonalCalculadora} alt="" />
+
+                  <div className="p-5 lg:m-5 text-left ">
+
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {personal.name} </h5>
+
+                    <p className="mb-3 font-normal max-w-sm text-gray-700 dark:text-gray-400 lg:py-5">
+                      {personal.description} <br />
+                      {personal.descriptionUno} <br />
+                      {personal.descriptionDos} <br />
+                    </p>
+
+                    <a href={personal.link} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#fff] bg-[#495057] rounded-lg focus:ring-blue-300  dark:hover:bg-blue-700 dark:focus:ring-blue-800 " rel="noreferrer" target="_blank" >
+                      ver pagina
+
+                      <svg className="w-[24px] h-[24px] ml-1 text-[#fff] " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+
+                      </svg>
+
+                    </a>
+
+
+                  </div>
+                </div>
+
+              </div>
+
+
+
+            ))}
+
+
+            {/* {proyectosPersonalesComponents.map((personal, index) => (
             
             <div key={index} className=" mb-5 mx-5 flex justify-center " >
 
@@ -317,11 +319,11 @@ export const Projects = () => {
             
       
           ))} */}
-              
-          
-          
 
-          {/* ----- */}
+
+
+
+            {/* ----- */}
 
           </div>
 
--- src/datos/MuroDeFirmas_ProyectoPersonal.js
+++ src/datos/MuroDeFirmas_ProyectoPersonal.js
@@ -1,10 +1,12 @@
 
+import muroDeFirmas from '../assets/images/MuroDeFirmas-viewMobile.png'
+import binaryAppImg from '../assets/images/binary-app.png'
 
 const MuroDeFirmas = [
 
     {
         name: "Muro de Firmas",
-        image: "/src/assets/images//MuroDeFirmas-viewMobile.png",
+        image: muroDeFirmas,
         description: "Proyecto desarrollado con React y Firebase.",
         descriptionUno: "Permite a los usuarios dejar su firma y una descripción personal que quedan almacenadas en la base de datos.",
         descriptionDos: "Utilice React-Toastify para notificaciones, TailwindCSS para estilos.",
@@ -12,6 +14,17 @@ const MuroDeFirmas = [
         descriptionCuatro: "Los visitantes pueden ver las firmas dejadas por otros usuarios al acceder al muro.",
         link: "https://muro-de-firmas.vercel.app/",
     },
+
+    {
+        name: "Binary App",
+        image: binaryAppImg,
+        description: "Los usuarios pueden convertir números de formato decimal a binario y viceversa.",
+        descriptionUno: "Herramienta educativa y práctica para estudiantes, educadores y profesionales interesados en la informática y matemáticas.",
+        descriptionDos: "Actualiza los resultados de la conversión en tiempo real, mejorando la experiencia del usuario.",
+        descriptionTres: "Utiliza hooks de React como useState para gestionar el estado de las entradas y las conversiones",
+        descriptionCuatro: "",
+        link: "https://binary-app.vercel.app/",
+    }
 
 ]
 
